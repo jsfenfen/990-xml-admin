a69a65715bd451bdbf9a8d4b8389859e5398d7bd
Jacob Fenton <jsfenfen@gmail.com>
2017-06-13 11:10:40 -0700

    add dependency

---
+++ irs990/filing/type_utils.py
@@ -0,0 +1,8 @@
+from collections import OrderedDict
+
+## Type names for simpler comparison 
+dictType=type(dict())
+orderedDictType=type(OrderedDict())
+listType=type(list())
+unicodeType=type(u'')
+noneType=type(None)
